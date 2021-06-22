b8c9d5728951002f07c46ad17d9a75c97775c295
ilyeongJeong <developjiy@gmail.com>
2021-06-23 00:01:34 +0900

    ref #1 vite config 확인중

--- vue-typescript-vite/vite.config.ts
+++ vue-typescript-vite/vite.config.ts
@@ -3,8 +3,21 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Base public path (default: '/')
+  base: '/',
+  // development for serve, production for build
+  mode: 'development',
+  // 참고 https://vitejs.dev/guide/api-plugin.html#conventions
   plugins: [vue()],
+  // Server Options
   server: {
+    // IP 주소를 지정 (default: '127.0.0.1')
+    host: '127.0.0.1',
+    // 서버 포트번호 지정
     port: 3000,
-  }
+    // 지정된 포트번호가 사용 중일경우 true이면 자동으로 다음 포트번호로 연결
+    // strictPort: true,
+    // Configure CORS for the dev server (Type: boolean | CorsOptions)
+    // cors: true,
+  },
 })
